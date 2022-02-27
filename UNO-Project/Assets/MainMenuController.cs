05231471b6805279a758c43c148fe47079c5305c
Gilloog <98500304+Gilloog@users.noreply.github.com>
2022-02-27 15:21:47 -0500

    Progress
    This is what I have got done so far

---
+++ UNO-Project/Assets/MainMenuController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MainMenuController : MonoBehaviour
+{
+    public void playGame(){
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void options(){
+
+    }
+
+    public void quitGame(){
+        Application.Quit();
+    }
+}
