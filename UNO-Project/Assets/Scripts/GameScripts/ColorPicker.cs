4e63d19a999a1a7bf4da4c98a89d7709bdec94c0
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-04-27 10:12:32 -0400

    update ColorPicker

---
+++ UNO-Project/Assets/Scripts/GameScripts/ColorPicker.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPicker : MonoBehaviour
+{
+
+    public GameObject colorPicker; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        colorPicker.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
