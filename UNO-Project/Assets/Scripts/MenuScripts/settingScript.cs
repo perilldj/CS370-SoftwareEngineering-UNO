97c9cc44a0f95676c71d8aa65c9accc5ff7b67a8
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-05-01 21:35:11 -0400

    Setting: updated the volume slider.

--- UNO-Project/Assets/Scripts/MenuScripts/settingScript.cs
+++ UNO-Project/Assets/Scripts/MenuScripts/settingScript.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.Audio;
 public class settingScript : MonoBehaviour
 {
+    public AudioMixer volumeController;
     public void Back () {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
+    public void SetVolume(float volume) {
+        volumeController.SetFloat("volume", volume);
     }
 }
