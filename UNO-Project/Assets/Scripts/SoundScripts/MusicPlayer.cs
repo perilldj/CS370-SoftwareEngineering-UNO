c3b6f52af01c0e32cc050c4b6d9a37f89c247ccb
Dean Perillo <perilldj@sunypoly.edu>
2022-05-04 23:31:14 -0400

    Added music player

---
+++ UNO-Project/Assets/Scripts/SoundScripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    public AudioClip[] clips;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start() {
+        audioSource.loop = false;
+
+    }
+
+    private AudioClip GetRandomClip() {
+        return clips[Random.Range(0, clips.Length)];
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(!audioSource.isPlaying) {
+            audioSource.clip = GetRandomClip();
+            audioSource.Play();
+        }
+    }
+}
